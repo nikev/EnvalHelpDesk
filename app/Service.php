43b867884846872c5c6a276c107ea5095a8f5621
nikev <ouattara630@gmail.com>
2020-01-13 16:22:32 +0000

    ajout de la propriété

--- app/Service.php
+++ app/Service.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
+
+
+    protected $fillable = [
+        'lib_serv',
+
+    ];
     public function employes()
     {
         return $this->hasMany(Employe::class);
--- app/Ticket.php
+++ app/Ticket.php
@@ -5,7 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
+
 {
+
+    protected $fillable = [
+        'cat_incident',
+        'niveau_sev',
+        'description',
+    ];
     public function employe()
     {
         return $this->belongsTo(Employe::class);
